7f25d8bd674496c4bc4c8556c400b3d2d9e005df
Koneva <sashakoneva@mail.ru>
2017-03-18 22:07:57 +0300

    all in one class

---
+++ sln/Modeling/Modeling/Form2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Modeling
+{
+    public partial class Form2 : Form
+    {
+        public float aver;  //average value
+        public float dis;   //dispersion
+        public int num;   //namber of people
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == null)
+            textBox1.Text = "0";
+            int x = Convert.ToInt32(textBox1.Text);
+            num = x;
+            label2.Text = Convert.ToString(num);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == null)
+                textBox1.Text = "0";
+            float x = (float)Convert.ToDouble(textBox2.Text);
+            dis = x;
+            label5.Text = Convert.ToString(dis);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == null)
+                textBox1.Text = "0";
+            float x = (float)Convert.ToDouble(textBox3.Text);
+            aver = x;
+            label6.Text = Convert.ToString(aver);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void raffle()
+        {
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            float[] raf = new float[num];
+            for (int i = 0; i < num - 1; i++)
+                raf[i] = rnd.Next(num);
+        }
+        
+    }
+}
---
+++ sln/Modeling/Modeling/Form3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Modeling
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConditionalFunc elem = new ConditionalFunc((float)Convert.ToDouble(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            for (int i = 0; i < Convert.ToInt32(textBox2.Text); i++)
+                elem.SetVal(elem.GetRandomValue());
+            elem.GetVal();
+            
+            for (int i = 0; i < Convert.ToInt32(textBox2.Text); i++)
+                textBox3.Text += Convert.ToString(elem.val[i]) + ' ' + ' ' + ' ';
+        }
+    }
+}
