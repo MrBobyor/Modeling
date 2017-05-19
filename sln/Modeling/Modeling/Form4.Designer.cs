02a704a5eec04f644f7fc0e120ae20a47d517111
MrBobyor <nano_enot@mail.ru>
2017-05-19 10:17:35 +0300

    all (not work the integrals)

--- sln/Modeling/Modeling/Form4.Designer.cs
+++ sln/Modeling/Modeling/Form4.Designer.cs
@@ -40,12 +40,19 @@
             this.label5 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(225, 42);
+            this.label1.Location = new System.Drawing.Point(12, 27);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(112, 13);
             this.label1.TabIndex = 0;
@@ -54,7 +61,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(225, 69);
+            this.label2.Location = new System.Drawing.Point(12, 53);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(123, 13);
             this.label2.TabIndex = 1;
@@ -62,7 +69,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(364, 39);
+            this.textBox1.Location = new System.Drawing.Point(151, 24);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 2;
@@ -70,7 +77,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(364, 66);
+            this.textBox2.Location = new System.Drawing.Point(151, 50);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 3;
@@ -78,14 +85,14 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(228, 105);
+            this.listBox1.Location = new System.Drawing.Point(301, 104);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.Size = new System.Drawing.Size(167, 173);
             this.listBox1.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(60, 172);
+            this.button1.Location = new System.Drawing.Point(79, 207);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
@@ -96,15 +103,15 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(79, 105);
+            this.label3.Location = new System.Drawing.Point(298, 87);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(76, 13);
             this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
+            this.label3.Text = "ИНТЕРВАЛЫ";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(23, 134);
+            this.textBox3.Location = new System.Drawing.Point(40, 163);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(62, 20);
             this.textBox3.TabIndex = 7;
@@ -112,7 +119,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(31, 69);
+            this.label4.Location = new System.Drawing.Point(57, 117);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 8;
@@ -121,7 +128,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(114, 68);
+            this.label5.Location = new System.Drawing.Point(148, 117);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(35, 13);
             this.label5.TabIndex = 9;
@@ -129,7 +136,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(117, 134);
+            this.textBox4.Location = new System.Drawing.Point(138, 163);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(63, 20);
             this.textBox4.TabIndex = 10;
@@ -137,16 +144,88 @@
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(364, 105);
+            this.listBox2.Location = new System.Drawing.Point(487, 104);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.Size = new System.Drawing.Size(134, 173);
             this.listBox2.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(100, 319);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "label6";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(487, 292);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Проверить гипотезу";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(484, 340);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "label7";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(484, 87);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "ГИПОТЕЗА";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(15, 87);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(223, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "ЗАДАТЬ РАЗБИЕНИЕ ДЛЯ ИНТЕРВАЛА :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(114, 117);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(10, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "-";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(103, 340);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(41, 13);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "label11";
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 323);
+            this.ClientSize = new System.Drawing.Size(645, 407);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label5);
@@ -160,7 +239,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form4";
-            this.Text = "Form4";
+            this.Text = "ПРОВЕРКА ГИПОТЕЗЫ О ВИДЕ РАСПРЕДЕЛЕНИЯ";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,5 +259,12 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label11;
     }
 }
--- sln/Modeling/Modeling/Form4.cs
+++ sln/Modeling/Modeling/Form4.cs
@@ -17,25 +17,24 @@ namespace Modeling
     public partial class Form4 : Form
     {
         GenValues elem;
+        double z0 ;
+        double zn;
         public Form4(double Z0,double Zn, GenValues Elem)
         {
             Form3 main = this.Owner as Form3;
             InitializeComponent();
-            double z0 = Z0;
-            double zn = Zn;
+            z0 = Z0;
+            zn = Zn;
             label4.Text = Convert.ToString(Math.Round(z0, 3));
             label5.Text = Convert.ToString(Math.Round(zn, 3));
             textBox3.Text = label4.Text;
             elem = new GenValues(Elem.sigma, Elem.num);
             elem.Clone(Elem);
         }
-
-        double alfa = 0;
-        double k = 0;
+        
         double[] q;
         double[] z;
-        double R0;
-        double F;
+        int[] nj;
 
         int flag = 1;
 
@@ -46,7 +45,7 @@ namespace Modeling
                 z = new double[Convert.ToInt32(textBox1.Text) + 1];
                 q = new double[Convert.ToInt32(textBox1.Text)];
             }
-            z[0] = Convert.ToDouble(label4.Text);
+            z[0] = Convert.ToDouble(z0);
         }
 
         //ввод промежутков и формирование массива промежутков
@@ -66,7 +65,7 @@ namespace Modeling
                 {
                     button1.Enabled = false;
                     textBox4.Text = label5.Text;
-                    z[flag] = Convert.ToDouble(textBox4.Text);
+                    z[flag] = Convert.ToDouble(zn);
                     str = "interval " + flag.ToString() + "    (" + textBox3.Text + " , " + textBox4.Text + ")";
                     listBox1.Items.Add(str);
                     //ГИПОТЕЗА
@@ -75,6 +74,42 @@ namespace Modeling
                         q[i - 1] += metodTrapezium1(z[i - 1], z[i]);
                         listBox2.Items.Add(Convert.ToString(q[i - 1]));
                     }
+                    button2.Enabled = true;
+                    label6.Text = Convert.ToString(valueRO());
+                    label11.Text = Convert.ToString(methodOfTrapezium2(valueRO()));
+                }
+            }
+        }
+
+        //мера расхождения
+        private double valueRO()
+        {
+            NumbersOfValuesInIntervalMas();
+            double R0 = 0;
+            for (int i = 0; i < Convert.ToInt32(textBox1.Text); i++)
+            {
+                R0 += ((Math.Pow((nj[i] - elem.num * q[i]), 2)) / (elem.num * q[i]));
+            }
+            return R0;
+        }
+
+        //значения на промежутках
+        private void NumbersOfValuesInIntervalMas()
+        {
+            nj = new int[Convert.ToInt32(textBox1.Text)];
+            for (int i = 0; i < Convert.ToInt32(textBox1.Text); i++)
+                nj[i] = 0;
+            int j = 0;
+            double zF = z[j + 1];
+            for (int i = 0; i < elem.num; i++)
+            {
+                if (elem.val[i] < zF)
+                    nj[j]++;
+                else
+                {
+                    j++;
+                    nj[j]++;
+                    zF = z[j + 1];
                 }
             }
         }
@@ -85,20 +120,81 @@ namespace Modeling
             int n = 1000;
             for (int i = 1; i <= n; i++)
             {
-                integral += (double)(elem.functionDisribution2((float)(a + (double)(b - a) * ((double)(i - 1) / (double)n)), elem.sigma) + elem.functionDisribution2((float)(a + (double)(b - a) * ((double)i / (double)n)), elem.sigma)) * (double)((double)b - (double)a) / (2.0 * (double)n);
+                integral += ((double)(elem.functionDisribution2((float)(a + (b - a) * ((i - 1) / n)), elem.sigma) + elem.functionDisribution2((float)(a + (b - a) * ( i / n)), elem.sigma)) *  (b - a) / (2.0 * n));
             }
             return integral;
         }
+        
+        private double methodOfTrapezium2(double R0)
+        {
+            double integral = 0;
+            int n = 1000;
+            for (int i = 1; i <= n; i++)
+            {
+                integral += (double)(densityFunctionOfChiDistribution(R0 * (i - 1) / n) + densityFunctionOfChiDistribution(R0 * i / n));
+            }
+            return (R0 / 2 * n) * integral;
+        }
+        
+        //плотность распределения chi2
+        public double densityFunctionOfChiDistribution(double x)
+        {
+            int r = Convert.ToInt32(textBox1.Text);
+            if (x > 0)
+                return Math.Pow(2, (-r / 2)) * Math.Pow(GammaFunction(r / 2), (-1)) * Math.Pow(x, (r / (2 - 1))) * Math.Exp(-x / 2);
+            else return 0;
+        }
 
-        //double MethodOfTrapezium2(double R0)
+        //private double funcFR0(double z1, double z2, int j)
         //{
-        //    double integral = 0;
-        //    int n = 1000;
-        //    for (int i = 1; i <= n; i++)
+        //    int r = Convert.ToInt32(textBox1.Text);
+        //    double sum = 0;
+        //    if (r == 0)
+        //        return 0;
+        //    for (int k = 1; k <= j; k++)
         //    {
-        //        integral += (double)(gFunction(R0 * (i - 1) / n) + gFunction(R0 * i / n));
+        //        sum += (densityFunctionOfChiDistribution(z1 + (z2 - z1) * ((k - 1) / j)) + densityFunctionOfChiDistribution(z1 + (z2 - z1) * (k / j)));
         //    }
-        //    return (R0 / 2 * n) * integral;
+        //    return (1 - ((z2 - z1) / (2 * j)) * sum);
         //}
+
+        private double GammaFunction(double value)
+        {
+            double gamma = 1;
+            if (value == 0)
+            {
+                return 1;
+            }
+            if ((int)value == value)
+            {
+                for (int i = 2; i < value; i++)
+                {
+                    gamma *= i;
+                }
+            }
+            else
+            {
+                gamma = (double)Math.Pow(Math.PI, 0.5);
+                for (double i = (1.0 / 2.0); i < value; i++)
+                {
+                    gamma *= i;
+                }
+            }
+            return gamma;
+        }
+
+        //ПРИНЯТИЕ.ОТКЛОНЕНИЕ
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double alfa = Convert.ToDouble(textBox2.Text);
+            if (alfa <= 0 || alfa > 1)
+                MessageBox.Show("Error", "Значение альфа должно принадлежать промежутку (0, 1]");
+            else
+            {
+                if (alfa > methodOfTrapezium2(valueRO()))
+                    label7.Text = "ПРИНЯТА";
+                else label7.Text = "ОТКЛОНЕНА";
+            }
+        }
     }
 }
